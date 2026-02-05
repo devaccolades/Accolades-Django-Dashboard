4d1c28551f0931f63e7cd678a41cdc56ec21be2a
Accolades Integrated <accoladesintegrated@Accoladess-iMac.local>
2026-02-05 14:08:14 +0530

    updated

--- DashboardDjango/settings.py
+++ DashboardDjango/settings.py
@@ -137,6 +137,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
